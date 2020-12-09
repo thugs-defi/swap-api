dd46e9cd1a114c9e939990c66458d923561c02d3
Theodore Thuggington <thugfi@protonmail.com>
2020-12-09 17:46:09 -0500

    api

---
+++ src/blocks/client.ts
@@ -0,0 +1,12 @@
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import fetch from 'node-fetch'
+
+export const blockClient = new ApolloClient({
+  link: new HttpLink({
+    fetch,
+    uri: 'https://api.bscgraph.org/subgraphs/name/bsc-blocks'
+  }),
+  cache: new InMemoryCache()
+})
